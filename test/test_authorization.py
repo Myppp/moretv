d3f5d3c65c173e6111870cc51a1d766360512a1c
Dmitriy Belkin <demetrius.belkin@gmail.com>
2021-02-12 17:02:04 +0200

    Первый тест на авторизацию

--- test/test_authorization.py
+++ test/test_authorization.py
@@ -7,27 +7,23 @@ import unittest, time, re
 
 class MoreTV(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
 
     def test_more_t_v(self):
         wd = self.wd
+        wd.set_window_size(1300, 960)
         wd.get("https://beta.more.tv/")
-        wd.find_element_by_link_text(u"войти").click()
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("more")
+        wd.find_element_by_xpath("//a[contains(text(),'войти')]").click()
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys("more3qabd+beta@yandex.ru")
-        wd.find_element_by_xpath("//div[@id='blackout-container']/div/div/div").click()
         wd.find_element_by_name("password").click()
         wd.find_element_by_name("password").clear()
         wd.find_element_by_name("password").send_keys("1217470")
         wd.find_element_by_xpath("//button[@type='submit']").click()
         wd.find_element_by_xpath("//img[@alt='arrow']").click()
-        wd.find_element_by_xpath(
-            "//div[@id='root']/div/div[2]/div/section/div/div[2]/div[2]/div/div[4]/div[2]/div/div/div").click()
+        wd.find_element_by_xpath("//div[contains(text(),'more3qabd+beta@yandex.ru')]")
 
     def is_element_present(self, how, what):
         try:
